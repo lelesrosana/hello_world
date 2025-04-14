3352d56c6eff9ed770f4729a821c9f82bf4c866c
Rosana Leles <97849886+lelesrosana@users.noreply.github.com>
2025-04-14 15:18:29 -0300

    Primeiro Commit Spring

---
+++ main/java/com/example/holloworld/HolloworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.holloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolloworldApplication.class, args);
+	}
+
+}
---
+++ test/java/com/example/holloworld/HolloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
