fbc10dc03c10d2498b7d132b05e2be18917100c3
Rosana Leles <97849886+lelesrosana@users.noreply.github.com>
2025-04-15 22:57:23 -0300

    Atualização do projeto

--- src/main/java/com/generation/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/generation/helloworld/controller/HelloWorldController.java
@@ -7,11 +7,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
-
+	
+	
 	@GetMapping
 	public String helloWorld() {
-		{
-			return "Hello World";
-		}
+		return "Hello World";
 	}
+	
+	
+	@GetMapping (value = "/bsm-lista", produces  = "text/plain")
+	public String bsmlista() {
+		return "Trabalho em equipe" 
+				+ "\nAdaptabilidade"
+				+ "\nInteligência emocional"
+				+"\nÉtica no trabalho"
+				+"\nComunicação assertiva"
+				+"\nGerir o tempo"
+				+"\nSer sincero";
+	}
+	
+	
+	@GetMapping (value = "/aprendizagem", produces = "text/plain")
+	public String aprendizagem() {
+		return ""
+				+"\nMySQL"
+				+"\nProtocolos e Camadas de aplicação"
+				+"\nMetodos get, pos, put e delet"
+				+"\nArquiteturas SOFEA, REST, MVC";
+	}
+	
+
 }
